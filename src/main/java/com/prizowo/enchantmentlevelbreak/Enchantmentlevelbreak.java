d72b8f3b76ce9100e21c27a46741c8cba8f5359f
xiaoliziawa <3682822069@qq.com>
2025-01-31 14:17:21 +0800

    update 1.3

--- src/main/java/com/prizowo/enchantmentlevelbreak/Enchantmentlevelbreak.java
+++ src/main/java/com/prizowo/enchantmentlevelbreak/Enchantmentlevelbreak.java
@@ -1,15 +1,20 @@
 package com.prizowo.enchantmentlevelbreak;
 
+import com.prizowo.enchantmentlevelbreak.config.Config;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 
 @Mod("enchantmentlevelbreak")
 public class Enchantmentlevelbreak {
     public static final String MODID = "enchantmentlevelbreak";
 
     public Enchantmentlevelbreak() {
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+        
         MinecraftForge.EVENT_BUS.register(this);
     }
 
